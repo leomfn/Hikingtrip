83932c5e63656abcbecf0b2e59c01f9a6a5b5ce5
luisa_von_wolffersdorff <luisa.vw@web.de>
2023-07-27 11:49:53 +0200

    new changes

--- main_lui_final.py
+++ main_lui_final.py
@@ -9,26 +9,26 @@ start_time = time.time()
 
 #load duration and distance matrix
 
-points = getWayPoints()
+"""points = getWayPoints()
 pointids = list(points.keys())
-dist, dur = getDistAndDur(pointids)
+dist, dur = getDistAndDur(pointids)"""
 
-"""dur = {1: {4: 1}, 2: {1: 4, 3: 3}, 3: {2: 1}, 4: {2: 2, 3: 2}}
-dist = {1: {2: 3, 3: 3}, 2: {3: 3, 4: 3}, 3: {4: 3}, 4: {2: 3, 3: 3}}"""
+dur = {1: {4: 1}, 2: {1: 4, 3: 3}, 3: {2: 1}, 4: {2: 2, 3: 2}}
+dist = {1: {2: 3, 3: 3}, 2: {3: 3, 4: 3}, 3: {4: 3}, 4: {2: 3, 3: 3}}
 
 """dur = {1: {2: 5, 3: 2}, 2: {3: 1, 4: 1}, 3: {4: 1}, 4: {2: 1, 3: 1}}
 dist = {1: {2: 3, 3: 3}, 2: {3: 3, 4: 3}, 3: {4: 3}, 4: {2: 3, 3: 3}}"""
 
-#load dictionary with edges (5 nearest or determined by previous runs
+"""#load dictionary with edges (5 nearest or determined by previous runs
 with open("2opt830_commonEdges.json") as json_file:
-    dict_edges = json.load(json_file)
+    dict_edges = json.load(json_file)"""
 
-"""dict_edges = {
+dict_edges = {
     "1": [4],
     "2": [1, 3],
     "3": [2],
     "4": [2, 3],
-}"""
+}
 """dict_edges = {
     "1": [2, 3],
     "2": [3, 4],
@@ -38,7 +38,7 @@ with open("2opt830_commonEdges.json") as json_file:
 
 #create permutations and save 2 shortest (considering the duration)
 
-starting_node = 81
+starting_node = 1
 tours = all_ways_to_visit_nodes_starting_node(dict_edges, starting_node, dur)
 
 for tour in tours:
